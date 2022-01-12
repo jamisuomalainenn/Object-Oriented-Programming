12cc59f3399f4f39ac683a47b55c24bfd779e853
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-01-12 15:02:48 +0200

    'Kirja' tehtävä aloitettu..

---
+++ BookExercise/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookExercise
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string id;
+        public double price;
+
+        //empty constructor
+        public Book()
+        {
+            this.title = string.Empty;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 19.95;
+        }
+        //constructor
+        public Book(string title)
+        {
+            this.title = title;
+            this.author = string.Empty;
+            this.id = string.Empty;
+            this.price = 19.95;
+
+        }
+
+        public Book(string title, string author, string id, double price) : this(title)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.price = price;
+        }
+    }
+}
--- BookExercise/Program.cs
+++ BookExercise/Program.cs
@@ -6,7 +6,16 @@ namespace BookExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Book exercise!");
+            Book book1 = new Book();
+            book1.title = "Matematiikka 1 " + "Salonen " + "Matti ";
+            Console.WriteLine("Ensimmäinen kirja: " + book1.title);
+
+            Book book2 = new Book("Aapinen " + "Salonen " + "Matti " + 22.50);
+            Console.WriteLine("Toinen kirja: " + book2.title);
+
+            Book book3 = new Book("Historia 4" + " Salonen" + " Matti " + 22.50);
+
         }
     }
 }
