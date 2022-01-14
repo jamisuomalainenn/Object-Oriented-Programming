dc40f46622c6cb1cec7daf868dd28ba70622cee9
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-01-14 11:11:11 +0200

    Book valmis.

--- BookExercise/Program.cs
+++ BookExercise/Program.cs
@@ -7,19 +7,19 @@ namespace BookExercise
         static void Main(string[] args)
         {
             Console.WriteLine("This is Book exercise!");
-            Book book1 = new Book("Matematiikka 1", "Salonen Matti", "12345", 15.99);
+            Book book1 = new Book("Matematiikka 1 ", "Salonen Matti", "00001", 15.99);
             /*book1.title = "Matematiikka 1";
             book1.author = "Salonen " + "Matti";
             book1.id = "00001";                         //Kommentoitu osio on korvattu nopeammalla
             book1.price = 15.99;*/                      //tavalla. Näin saadaan koodi yhdelle riville ja saadaan tilaa
             //book1.SetId("joops");
-            Console.WriteLine("Ensimmäinen kirja: " + book1.title);
+            Console.WriteLine("Ensimmäinen kirja: " + book1.title + book1.id);
             
             Book book2 = new Book("Aapinen", "Aaltonen Antti", "00002", 20.00);
-            Console.WriteLine("Toinen kirja: " + book2.title);
+            Console.WriteLine("Toinen kirja: " + book2.title + book2.id);
 
             Book book3 = new Book("Witcher", "Rosa Nurminen", "00003", 25.90);
-            Console.WriteLine("Kolmas kirja: " + book3.title);
+            Console.WriteLine("Kolmas kirja: " + book3.title + book3.id);
             //Console.WriteLine(book3.GetBookInformation());
 
             book3.CompareBook(book2);
