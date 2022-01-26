8e6cbf784afe5750c0d4a9fc79f5bf09fc4fe73c
jamisuomalainenn <Jami.Suomalainen@student.lab.fi>
2022-01-26 14:23:14 +0200

    Book: 26.1, vaiheessa.

---
+++ BookAndAuthor/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookAndAuthor
+{
+    class Book
+    {
+        private string name;
+        private string author;
+        private string publisher;
+        private double price;
+        private string isbn;
+        public static string themeName;
+        //private int id;
+        //public static int counter = 1;
+
+
+        public Book()
+        {
+            this.Name = string.Empty;
+            this.author = string.Empty;
+            this.publisher = string.Empty;
+            this.price = 0;
+            this.isbn = string.Empty;
+        }
+
+        public string GetBookDetails()
+        {
+            {
+                return $"{this.name} {this.author} {this.isbn} {this.price:F2}";
+            }
+        }
+
+        public Book(string name)
+        {
+            this.Name = name;
+            this.isbn = "13";
+            //counter++;
+        }
+
+        public void SetIsbn(string isbnValue)
+        {
+            //tarkista tässä onko ISBN 13 merkkiä pitkä
+            if (isbnValue.Length == 13)
+            {
+                this.isbn = isbnValue;
+            }
+            else
+            {
+                this.isbn = "Not valid, set 5 number ID";
+            }
+        }
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value.Length > MaxLength)
+                {
+                    Console.WriteLine("Nimi on liian pitkä.");
+                }
+                else
+                {
+                    name = value.ToLower();
+                }
+            }
+        }
+
+        //public int Id {
+        //    get { return id; }
+        //    set { id = value; }
+        
+    }
+}
---
+++ BookAndAuthor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BookAndAuthor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tehtävä Book!");
+
+            Book book1 = new Book("Aapinen");
+            book1.Name = "ABC";
+
+            Book book2 = new Book("Witcher");
+
+            Console.WriteLine($"kirjan nimi on: {book1.Name}");
+            //Console.WriteLine($"Book-luokan counter = {Book.counter}");
+
+            //Console.WriteLine($"{book2.Id}. kirjan nimi on: {book2.Name}");
+            //Console.WriteLine($"Book-luokan counter = {Book.counter}");
+        }
+    }
+}
